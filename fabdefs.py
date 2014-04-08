458031d32518885631b2132977a554438e0fae34
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-04-08 14:00:52 +0200

    First deploy of basic app to production server.

--- fabdefs.py
+++ fabdefs.py
@@ -15,6 +15,7 @@ def staging():
     env.project_dir = '/var/www/project-template'  # the directory where the application resides on the server
     env.config_dir = 'config/staging'  # the local directory where the config files for this server is kept
     env.env_dir = env.project_dir + "/env"  # path to this app's virtualenv (important if it runs on a shared server)
+    env.activate = 'source %s/env/bin/activate' % env.project_dir
     return
 
 
--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,10 @@ from fabdefs import *
 from fabric.api import *
 from contextlib import contextmanager
 
-# hook for activating a virtualenv on the server
-env.activate = 'source %s/env/bin/activate' % env.code_dir
 
 @contextmanager
 def virtualenv():
-    with cd(env.code_dir):
+    with cd(env.project_dir):
         with prefix(env.activate):
             yield
 
