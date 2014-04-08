d446bfcca8870adc046745ab5bfe506cec0f5966
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-04-08 21:54:43 +0200

    Separate the genders.

--- backend/views.py
+++ backend/views.py
@@ -10,8 +10,14 @@ with app.open_instance_resource('mp_details.json') as f:
     mp_details = json.loads(f.read())
 
     mp_list = []
+    males = []
+    females = []
     for key, val in mp_details.iteritems():
         mp_list.append(key)
+        if val["gender"] == "male":
+            males.append(key)
+        else:
+            females.append(key)
 
 
 def save_data():
@@ -22,14 +28,22 @@ def save_data():
     return
 
 
-@app.route('/get_member/')
-def get_member():
+@app.route('/get_member/<gender>/')
+def get_member(gender):
     """
     Return the details of a randomly selected member of parliament.
     """
 
-    i = randint(0, len(mp_details)-1)
-    return jsonify({"id": mp_list[i], "data": mp_details[mp_list[i]]})
+    if not gender.lower() in ["male", "female"]:
+        abort(400)
+
+    if gender.lower() == "male":
+        tmp = males
+    else:
+        tmp = females
+
+    i = randint(0, len(tmp)-1)
+    return jsonify({"id": tmp[i], "data": mp_details[tmp[i]]})
 
 
 @app.route('/hot/<mp_id>/')
@@ -69,18 +83,28 @@ def not_hot(mp_id):
 @app.route('/ranking/')
 def ranking():
     """
-    Return the 20 highest ranked MP's.
+    Return the 10 highest ranked MP's of each gender.
     """
 
     tmp = []
-    for mp_id in mp_list:
+    for mp_id in males:
         tmp.append({'mp_id': mp_id, 'score': mp_details[mp_id]['score']})
     tmp = sorted(tmp, key=itemgetter('score'))
     tmp.reverse()
-    out = {}
-    for i in range(min(tmp, 10)):
+    top_males = {}
+    for i in range(min(len(tmp), 10)):
         rec = tmp[i]
         mp = {"id": rec['mp_id'], "data": mp_details[rec['mp_id']]}
-        out[i+1] = mp
-    print out
-    return jsonify(out)
+        top_males[i+1] = mp
+
+    tmp = []
+    for mp_id in females:
+        tmp.append({'mp_id': mp_id, 'score': mp_details[mp_id]['score']})
+    tmp = sorted(tmp, key=itemgetter('score'))
+    tmp.reverse()
+    top_females = {}
+    for i in range(min(len(tmp), 10)):
+        rec = tmp[i]
+        mp = {"id": rec['mp_id'], "data": mp_details[rec['mp_id']]}
+        top_females[i+1] = mp
+    return jsonify({"male": top_males, "female": top_females})
